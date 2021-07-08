f0346bbe4a6961db70bfd36c2b30a7ab97eb7d41
Javokhirbek2000 <j.mirzaakbarov.2000@gmail.com>
2021-07-08 23:29:48 +0500

    Initial commit

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const express=require("express");
 const bodyParser=require("body-parser");
 const ejs=require("ejs");
 const mongoose=require("mongoose");
-const encrypt=require("mongoose-encryption");
+const bcrypt=require("bcrypt");
+const saltRounds=10;
 
 const app=express();
 
@@ -19,8 +20,6 @@ const userSchema=new mongoose.Schema({
 });
 
 
-userSchema.plugin(encrypt, { secret: process.env.secret, encryptedFields: ['password'] });
-
 const User=mongoose.model('User', userSchema);
 
 app.get('/',function(req,res){
@@ -34,32 +33,33 @@ app.get('/register',function(req,res){
 });
 
 app.post("/register", function(req,res){
-    const newUser=new User ({
-        email: req.body.username,
-        password: req.body.password
-    });
-    newUser.save(function(err){
-        if (err) {
-            console.log(err);
-        } else {
-            res.render("secrets");
-        }
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        const newUser=new User ({
+            email: req.body.username,
+            password: hash
+        });
+        newUser.save(function(err){
+            if (err) {
+                console.log(err);
+            } else {
+                res.render("secrets");
+            }
+        });
     });
 });
 app.post("/login", function(req,res){
     const username=req.body.username;
-    const password=req.body.password;
 
     User.findOne({email: username}, function(err, foundUser){
         if (err) {
             console.log(err);
         } else {
-            if( foundUser.password === password) {
-                res.render("secrets");
-            }
+            const hash=foundUser.password;
+            bcrypt.compare(req.params.password, hash);
+            
         }
-    } )
-})
+    });
+});
 
 
 app.listen(3000, function(){
